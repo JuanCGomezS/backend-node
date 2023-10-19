e6f631111ee97778b5b1a00eda446f0009e84fad
Juan Carlos Gomez <juan.gomez@synergias.co>
2023-10-19 18:58:17 -0500

    Inclusion de BD en Mysql

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 module.exports = {
-    api: {
-        port: process.env.API_PORT || 3000,
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET || 'notasecret!',
-    },
-}
+  api: {
+    port: process.env.API_PORT || 3000,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || "notasecret!",
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || "127.0.0.1",
+    port: process.env.MYSQL_PORT || "3306",
+    user: process.env.USER || "root",
+    password: process.env.MYSQL_PASSWORD || "12345678",
+    database: process.env.MYSQL_DATABASE || "developernode",
+  },
+};
